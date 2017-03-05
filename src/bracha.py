d2fc9dcf097104f7b0f63797805097260572361f
kc1212 <kc1212@users.noreply.github.com>
2017-03-05 22:41:48 +0100

    Remove round in Bracha

--- src/bracha.py
+++ src/bracha.py
@@ -11,7 +11,6 @@ class Bracha:
     def __init__(self, factory):
         self.factory = factory
         self.step = BrachaStep.one
-        self.round = -1
         self.init_count = 0
         self.echo_count = 0
         self.ready_count = 0
@@ -26,7 +25,6 @@ class Bracha:
         msg should be in the following format
         struct Msg {
             ty: u32,
-            round: u32,
             body: String,
         }
         :param msg:
@@ -36,26 +34,13 @@ class Bracha:
             print "Bracha is done, doing nothing"
             return
 
-
         print "received: ", msg
         ty = msg["ty"]
-        round = msg["round"]
         body = msg["body"]
 
-        assert isinstance(round, int)
         assert isinstance(ty, int)
 
-        if round < 0:
-            print "bracha: invalid round", round
-            return
-
-        if ty != MsgType.init.value and round != self.round:
-            # NOTE this is often ok, since the value may already be accepted
-            print "bracha: found invalid message", msg
-            return
-
         if ty == MsgType.init.value:
-            self.round = round  # TODO make sure rounds do not overlap
             self.init_count += 1
         elif ty == MsgType.echo.value:
             self.echo_count += 1
@@ -78,21 +63,19 @@ class Bracha:
         if self.step == BrachaStep.three:
             if self.enough_ready():
                 self.step = BrachaStep.one
-                self.round = -1
                 self.init_count = 0
                 self.done = True
                 print bcolors.OKGREEN + "bracha: ACCEPT" + bcolors.ENDC, body
 
-    def bcast_init(self):
-        self.round = random.randint(0, 9999)
-        self.bcast(make_init(self.round, "agree this " + str(random.random())))
-        print "initiating round", self.round
+    def bcast_init(self, msg="some test msg!!"):
+        self.bcast(make_init(msg))
+        print "initiating with msg", msg
 
     def bcast_echo(self, body):
-        self.bcast(make_echo(self.round, body))
+        self.bcast(make_echo(body))
 
     def bcast_ready(self, body):
-        self.bcast(make_ready(self.round, body))
+        self.bcast(make_ready(body))
 
     def ok_to_send(self):
         n = self.factory.config.n
@@ -114,17 +97,17 @@ class Bracha:
         self.factory.bcast(msg)
 
 
-def make_init(round, body):
-    return _make_msg(MsgType.init.value, round, body)
+def make_init(body):
+    return _make_msg(MsgType.init.value, body)
 
 
-def make_echo(round, body):
-    return _make_msg(MsgType.echo.value, round, body)
+def make_echo(body):
+    return _make_msg(MsgType.echo.value, body)
 
 
-def make_ready(round, body):
-    return _make_msg(MsgType.ready.value, round, body)
+def make_ready(body):
+    return _make_msg(MsgType.ready.value, body)
 
 
-def _make_msg(ty, round, body):
-    return Payload.make_bracha({"ty": ty, "round": round, "body": body}).to_dict()
+def _make_msg(ty, body):
+    return Payload.make_bracha({"ty": ty, "body": body}).to_dict()
